4a8258d81cde9a78968430e9576b6089cb688886
dhruv <dhruvrajput1904200@gmail.com>
2025-03-17 22:06:25 +0530

    add bird

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import time
-
+from bird import Bird
 pg.init()
 
 class Game:
@@ -13,7 +13,8 @@ class Game:
         self.win = pg.display.set_mode((self.width, self.height))
         self.clock = pg.time.Clock()
         self.move_speed = 100 
-
+        self.bird = Bird(self.scale_factor)
+        self.is_enter_pressed = False
         self.setUPBgAndGround()
         self.gameLoop()
 
@@ -27,6 +28,11 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_RETURN :
+                        self.is_enter_pressed = True
+                    if event.key == pg.K_SPACE and self.is_enter_pressed:
+                        self.bird.flap(dt)
 
             self.updateEverything(dt)
             self.draw()
@@ -39,15 +45,18 @@ class Game:
 
     def updateEverything(self, dt):
         # Move ground left
-        self.ground1_rect.x -= self.move_speed * dt
-        self.ground2_rect.x -= self.move_speed * dt
+        if self.is_enter_pressed:
+            self.ground1_rect.x -= self.move_speed * dt
+            self.ground2_rect.x -= self.move_speed * dt
 
-        # Wrap ground when off-screen
-        if self.ground1_rect.right <= 0:
-            self.ground1_rect.x = self.ground2_rect.right
-        if self.ground2_rect.right <= 0:
-            self.ground2_rect.x = self.ground1_rect.right
-
+            # Wrap ground when off-screen
+            if self.ground1_rect.right <= 0:
+                self.ground1_rect.x = self.ground2_rect.right
+            if self.ground2_rect.right <= 0:
+                self.ground2_rect.x = self.ground1_rect.right
+            
+            self.bird.update(dt)    
+        
     def setUPBgAndGround(self):
         # Loading images for bg and ground
         self.bg_img = pg.transform.scale_by(pg.image.load("assets/bg.png").convert(), self.scale_factor)
@@ -67,6 +76,7 @@ class Game:
         self.win.blit(self.bg_img, (0, -300))
         self.win.blit(self.ground1_img, self.ground1_rect)
         self.win.blit(self.ground2_img, self.ground2_rect)
+        self.win.blit(self.bird.image,self.bird.rect)
 
 # Run only in main.py  
 if __name__ == "__main__":
